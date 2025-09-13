a36557bd792072aaa86e8127fadcfffb2c3b9530
Teacher Egg <teacherEgg@teacheregg.com>
2025-09-13 20:48:34 +0800

    4.5 整合 Tailwind CSS

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-// import './assets/main.css' // 引入 main.css 样式文件
+import '@/assets/main.css' // 引入 main.css 样式文件
 
 import { createApp } from 'vue'// 引入 createApp 方法
 import App from '@/App.vue'// 引入 App.vue 组件
