cc5aeecbc8e3d9655bb19cdf7ecf63ad84fc596d
Teacher Egg <teacherEgg@teacheregg.com>
2025-09-14 19:20:17 +0800

    5.1 登录页设计：支持响应式布局

--- src/main.js
+++ src/main.js
@@ -3,12 +3,19 @@ import '@/assets/main.css' // 引入 main.css 样式文件
 import { createApp } from 'vue'// 引入 createApp 方法
 import App from '@/App.vue'// 引入 App.vue 组件
 import router from '@/router/index.js'
+// 导入 Element Plus 图标
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-// 创建应用，并将 App 根组件挂载到 <div id="#app"></div> 中
-// createApp(App).use(router).mount('#app')
 
 const app = createApp(App)
+
+
+// 引入图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
 
 // 应用路由
 app.use(router)
 app.mount('#app')
+
